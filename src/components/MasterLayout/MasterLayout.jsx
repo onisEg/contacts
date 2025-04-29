0adb11f863ba1cdfb5a42e40150f1a4b8111604b
anas abdo <anasabdo704@gmail.com>
2025-04-29 13:41:48 +0200

    update

--- src/components/MasterLayout/MasterLayout.jsx
+++ src/components/MasterLayout/MasterLayout.jsx
@@ -4,7 +4,7 @@ import UserList from "../UserList/UserList";
 export default function MasterLayout() {
   return (
     <>
-      <div className=" bg-dark bg-opacity-25 col-md-8 border rounded-4 m-auto">
+      <div className=" bg-dark bg-opacity-25 col-md-8 border rounded-4 m-auto p-md-5">
         <div className="input-group input-group-lg px-4 pt-4">
           <input
             type="text"
