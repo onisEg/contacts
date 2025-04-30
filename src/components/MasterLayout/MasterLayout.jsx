42a2a8c615dbbd5242b2794e0327c155dc30bde3
anas abdo <anasabdo704@gmail.com>
2025-04-30 12:39:42 +0200

    background blur

--- src/components/MasterLayout/MasterLayout.jsx
+++ src/components/MasterLayout/MasterLayout.jsx
@@ -4,8 +4,14 @@ import UserList from "../UserList/UserList";
 export default function MasterLayout() {
   return (
     <>
-      <div className=" bg-dark bg-opacity-25 col-md-8 border rounded-4 m-auto p-md-5">
-        
+      <div
+        className="col-md-8 border rounded-4 m-auto p-md-5"
+        style={{
+          background: "rgba(255, 255, 255, 0.1)",
+          backdropFilter: "blur(10px)",
+          border: "1px solid rgba(255, 255, 255, 0.2)",
+        }}
+      >
         <UserList />
       </div>
     </>
